ae6c3aeee98780b379f0bdb64fe9e8b73c8fe2e0
Fabricio Ronchi <me@fabricioronchi.com>
2016-05-28 15:32:14 -0300

    Create SC-Summit-Sábado.js

---
+++ SC-Summit-Sábado.js
@@ -0,0 +1,35 @@
+9:30 Maujor
+* Flexbox
+  Block - Alinhamento vertical;
+  Inline - Alinhamento horizontal;
+  Flex layout - Revolução?
+  Determinação do início e fim;
+  Elementos se espandem de acordo com o container;
+  Bye Bye float e clear;
+
+10:45 Diego Faria
+* Javascript
+  Pegadinhas de contexto;
+
+11:15 Rafael Cianci
+* Pure Functions
+  Exemplos de pure functions;
+  Como trabalhar;
+  Genérico;
+
+13:30 Rodolfo A. e Gabriel P.
+* Reduxing your problems
+  React - Cuidado! Não chegue perto. Eu que digo.
+    Componentização;
+    Renderização;
+    Árvore de componentes;
+    DOM;
+    Introduction on React (book);
+  Flux
+    Arquitetura FrontEnd;
+    Não é MVC;
+    Fluxo unidirecional;
+  Redux
+    Single store;
+    Store confiável;
+    Programação funcional;
